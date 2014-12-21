3964d85fc9fe63d7eafa62ec36f790807033f451
David <davidyen1124@gmail.com>
2014-12-22 02:15:57 +0800

    0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='asyncio-yify',
-    version='0.0.3',
+    version='0.0.4',
     author='davidyen1124',
     author_email='davidyen1124@gmail.com',
     description='Wrapper for retrieving latest, popular movies and search movies on yify-torrent.org',
