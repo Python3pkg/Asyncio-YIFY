4a0802c2ffacb59e751d362158fd0648918a560e
David <davidyen1124@gmail.com>
2014-12-22 17:42:08 +0800

    remove requests from setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
     url='https://github.com/davidyen1124/Asyncio-YIFY',
     packages=['yify'],
     install_requires=[
-        'requests==2.5.0',
         'aiohttp==0.11.0',
         'lxml==3.4.1',
     ],
